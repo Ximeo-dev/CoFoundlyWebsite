fcf843bd3ba2b6aaaf66ff12fe1a6ddafdbf421d
amazonooo <garikogannisyan.9@gmail.com>
2025-04-19 19:14:10 +0300

    remove react-query from files

--- src/providers/query-provider.tsx
+++ src/providers/query-provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ReactNode, useState } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
