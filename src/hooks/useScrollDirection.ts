d432e7ee2f8b8f656031bbdf928ddbdeb65fb180
Ximeo-dev <ov3rskilled@gmail.com>
2025-05-01 02:54:43 +0300

    Header safari fix

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -3,9 +3,18 @@ import { useEffect, useState } from 'react'
 export const useScrollDirection = () => {
 	const [scrollDirection, setScrollDirection] = useState<'up' | 'down'>('up')
 	const [lastYScroll, setLastYScroll] = useState(0)
+	const threshold = 5
 
 	const handleScroll = () => {
 		const currentScrollY = window.scrollY
+		const maxScroll = document.documentElement.scrollHeight - window.innerHeight
+
+		if (currentScrollY < 0 || currentScrollY > maxScroll) return
+
+		const diff = currentScrollY - lastYScroll
+
+		if (Math.abs(diff) < threshold) return
+
 		if (currentScrollY > lastYScroll) {
 			setScrollDirection('down')
 		} else {
