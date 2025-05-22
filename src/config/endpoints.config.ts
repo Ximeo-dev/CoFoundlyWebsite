79626e2764a950b74e3b9d4cf7dd938ae7e51776
Ximeo <ov3rskilled@gmail.com>
2025-05-22 06:59:54 +0300

    App middleware

--- src/config/endpoints.config.ts
+++ src/config/endpoints.config.ts
@@ -1,13 +1,14 @@
 export const ENDPOINTS = {
-  HOME: '/',
-  LOGIN: '/login',
-  REGISTER: '/register',
-  PROFILE: '/profile',
+	HOME: '/',
+	WELCOME: '/welcome',
+	LOGIN: '/login',
+	REGISTER: '/register',
+	PROFILE: '/profile',
 	IDEAS: '/ideas',
 	SWIPE_USERS: '/swipes',
 	SUPPORT: '/support',
 	TERMS: '/terms',
 	PRIVACY: '/privacy',
 	PRICING: '/pricing',
-	CHATS: '/chats'
+	CHATS: '/chats',
 } as const
