9f003ed3ba78859461a0b2eca3331ed7a5f48e26
Ximeo-dev <ov3rskilled@gmail.com>
2025-05-23 02:49:35 +0300

    Websocket auto reconnect with token update

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,11 +1,10 @@
 import { SocketContext } from '@/providers/socket-provider'
 import { useContext } from 'react'
-import { Socket } from 'socket.io-client'
 
-export const useSocket = (): Socket => {
+export const useSocket = () => {
 	const socket = useContext(SocketContext)
 	if (!socket) {
-		throw new Error('useSocket must be used within a SocketProvider')
+		throw new Error('useSocket должен вызываться внутри SocketProvider')
 	}
 	return socket
 }
--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,22 +1,31 @@
-'use client'
-
-import { io, Socket } from 'socket.io-client'
 import { WS_URL } from '@/constants/api.constants'
-import { getAccessToken } from '@/services/auth-token.services'
+import { io, Socket } from 'socket.io-client'
 
 let socket: Socket | null = null
 
-export const getSocket = (): Socket => {
+/**
+ * Инициализирует или возвращает существующий экземпляр Socket.IO.
+ * @param token - JWT для авторизации при подключении
+ */
+export function getSocket(token: string): Socket {
 	if (!socket) {
 		socket = io(WS_URL, {
-			// path: '/socket.io',
-			transports: ['websocket'],
-			withCredentials: true,
-			auth: {
-				token: getAccessToken(),
-			},
+			auth: { token },
+			reconnection: true,
+			reconnectionAttempts: Infinity,
+			reconnectionDelay: 1000,
+			autoConnect: false,
+		})
+
+		socket.on('connect', () => console.log('✅ Socket connected:', socket?.id))
+
+		socket.on('disconnect', reason =>
+			console.warn('⚠️ Socket disconnected:', reason)
+		)
+
+		socket.on('errors', err => {
+			console.error('❌ Socket error:', err)
 		})
 	}
-
 	return socket
 }
