11d9acfadb8df8879cf41aa3b24b80f48e1e94e0
Ximeo-dev <ov3rskilled@gmail.com>
2025-05-23 04:11:47 +0300

    Socket auto reconnect fix

--- src/providers/socket-provider.tsx
+++ src/providers/socket-provider.tsx
@@ -2,29 +2,31 @@
 
 import React, { createContext, useContext, useEffect, useState } from 'react'
 import { getSocket } from '@/lib/socket'
-import type { Socket } from 'socket.io-client'
+import { Socket } from 'socket.io-client'
 import { getAccessToken } from '@/services/auth-token.services'
 import { authService } from '@/services/auth.service'
 
 export const SocketContext = createContext<Socket | null>(null)
 
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
-	const token = getAccessToken() ?? ''
-	const socket = getSocket(token)
-	socket.auth = { token: token }
-	socket.connect()
+	const [token, setToken] = useState<string>(getAccessToken() ?? '')
+	const [socket, setSocket] = useState<Socket>(getSocket(token))
 
 	useEffect(() => {
 		let isMounted = true
+		socket.auth = { token }
+		socket.connect()
 
 		socket.on('disconnect', async reason => {
 			console.log('Socket disconnected', reason)
 			if (!isMounted) return
 			try {
 				const response = await authService.getNewTokens()
-				const newToken = response.data.accessToken
-				socket.auth = { newToken }
-				setTimeout(() => socket.connect(), 2000)
+				setToken(response.data.accessToken)
+				socket.auth = { token }
+				setTimeout(() => {
+					socket.connect()
+				}, 2000)
 			} catch (err) {
 				console.error('Reauth failed', err)
 			}
